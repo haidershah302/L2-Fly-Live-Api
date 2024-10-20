21e825738496063e99bc8c97a0258352ac94138c
Haider Shah <stfox302@gmail.com>
2024-10-20 09:46:21 +0500

    testing deployment by changing web.php

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,22 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return redirect('/auth/google/redirect');
 });
 
 
 
-Route::get('/auth/google/redirect', function () {
+Route::get('/auth/google/redirect', function (Request $request) {
     return Socialite::driver('google')->redirect();
 });
 
 
-route::get('/auth/google/callback', function () {
-    dd('http://127.0.0.1:8000/auth/google/callback');
+route::get('/auth/google/callback', function (Request $request) {
+    dd(Socialite::driver('google')->user());
 });
 
 
